bc071e4424b8c7ebdc0af988e2b2e924d7ad4495
Jesse L. Zamora <xtremekforever@gmail.com>
2025-11-30 01:43:04 +0000

    Use main branch of swiftlang/swift-sdk-generator

--- build-generator.sh
+++ build-generator.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-SDK_GENERATOR_BRANCH=${SDK_GENERATOR_BRANCH:=develop}
-SDK_GENERATOR_REPO=${SDK_GENERATOR_REPO:=https://github.com/xtremekforever/swift-sdk-generator.git }
+SDK_GENERATOR_BRANCH=${SDK_GENERATOR_BRANCH:=main}
+SDK_GENERATOR_REPO=${SDK_GENERATOR_REPO:=https://github.com/swiftlang/swift-sdk-generator.git }
 SDK_GENERATOR_DIR=${SDK_GENERATOR_DIR:=swift-sdk-generator}
+
+# Dependencies
+if [ "$(grep -Ei 'debian' /etc/os-release)" ]; then
+    sudo apt install -y git-extras
+fi
 
 # Checkout
 git force-clone -b $SDK_GENERATOR_BRANCH $SDK_GENERATOR_REPO $SDK_GENERATOR_DIR || true
