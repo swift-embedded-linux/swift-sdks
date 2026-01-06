61f9d9add5cb38588946be64a685ad438b165390
Jesse L. Zamora <xtremekforever@gmail.com>
2026-01-06 02:11:08 +0000

    Remove test-sswg-packages test project
     - We don't use this, let's remove it.

--- test-sswg-packages/Sources/sswg-sandbox-packages/main.swift
+++
@@ -1,8 +0,0 @@
-import AWSLambdaRuntime
-import DiscordBM
-import Logging
-import MQTTNIO
-import RediStack
-
-let logger = Logger(label: "sswg-sandbox-packages")
-logger.info("Hello SSWG Sandbox Packages!")
